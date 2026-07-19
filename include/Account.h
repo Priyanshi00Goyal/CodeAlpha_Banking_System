a7c6eef44b876a9322d50c6ba6d96a93f5ca6240
Priyanshi Goyal <p.25012008.g@gmail.com>
2026-07-19 14:04:35 +0530

    Add Account class with basic functionality

--- include/Account.h
+++ include/Account.h
@@ -1,1 +1,48 @@
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Account
+{
+private:
+    // Static member to generate unique Account Numbers
+    static int nextAccountNumber;
+
+    int accountNumber;
+    int customerID;
+    string accountType;
+    double balance;
+
+public:
+    // Constructors
+    Account();
+    Account(int accNo, int custID, const string &type, double bal);
+
+    // Destructor
+    ~Account() = default;
+
+    // Setters
+    void setAccountNumber(int accNo);
+    void setCustomerID(int custID);
+    void setAccountType(const string &type);
+    void setBalance(double bal);
+
+    // Getters
+    int getAccountNumber() const;
+    int getCustomerID() const;
+    string getAccountType() const;
+    double getBalance() const;
+
+    // Member Functions
+    void createAccount();
+    void displayAccount() const;
+
+    void deposit(double amount);
+    bool withdraw(double amount);
+};
+
+#endif // ACCOUNT_H
